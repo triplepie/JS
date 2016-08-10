7aaf239f98ba033c405e277d88dae59c92ade45a
Alan Lenkovskiy <alanlenkovskiy8@gmail.com>
2016-08-10 08:53:59 -0400

    showed how to make a function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-var hi = "hello"//this is a variable
-alert(4) // this alerts taht user of the webpage of whatever developer wants to tell them
+var hi = "hello";//this is a variable
+alert(4); // this alerts taht user of the webpage of whatever developer wants to tell them
 //float, String, Interger, Boolean, Array, and object
-var hello = ["hi","how","Are", "You", "Doing", "?"]//this is an array
-alert(hello)// this is a form of testing because you'll c if everything workedon the website
+var hello = ["hi","how","Are", "You", "Doing", "?"];//this is an array
+alert(hello);// this is a form of testing because you'll c if everything workedon the website
+// some of the logical operators are == equal, > greater then, < less then, =! not equal to, <= less then or equal to, >= greater then or equal to etc.
+//another thing that you could do is functions which makes a new function to use;
+
+var number1 = (5);
+var number2 = (20);
+
+var x = myFunction(4, 3);        // Function is called, return value will end up in x
+
+function myFunction(a, b) {
+     alert(a * b);                // Function returns the product of a and b
+}
