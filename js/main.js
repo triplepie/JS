4800f8c13b8edba9d0cbc14ced7410c517210447
Alan Lenkovskiy <alanlenkovskiy8@gmail.com>
2016-08-10 08:42:42 -0400

    added all the data types, how to test, and how to make arraays

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 var hi = "hello"//this is a variable
 alert(4) // this alerts taht user of the webpage of whatever developer wants to tell them
+//float, String, Interger, Boolean, Array, and object
+var hello = ["hi","how","Are", "You", "Doing", "?"]//this is an array
+alert(hello)// this is a form of testing because you'll c if everything workedon the website
